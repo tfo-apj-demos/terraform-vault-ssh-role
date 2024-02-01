34781613957496f25fd27a9be156f6ae1e8d0528
Grant Orchard <go@hashicorp.com>
2024-02-01 17:30:37 +1100

    fix db name attribute config

--- main.tf
+++ main.tf
@@ -36,6 +36,6 @@ resource "vault_database_secret_backend_role" "this" {
   for_each = { for role in var.database_roles: role.name => role }
   backend = var.vault_mount_postgres_path
   name    = each.value.name
-  db_name = vault_database_secret_backend_connection.this.name
+  db_name = var.database_name
   creation_statements = each.value.creation_statements
 }
