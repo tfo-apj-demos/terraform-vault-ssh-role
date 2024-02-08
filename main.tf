a5fbdedc7e35e2c1831349d3d2219a9aab247e45
Grant Orchard <go@hashicorp.com>
2024-02-08 16:13:35 +1100

    add policies for reading creds

--- main.tf
+++ main.tf
@@ -42,3 +42,14 @@ resource "vault_database_secret_backend_role" "this" {
   db_name = vault_database_secret_backend_connection.this.name
   creation_statements = each.value.creation_statements
 }
+
+resource "vault_policy" "this" {
+  for_each = { for role in vault_database_secret_backend_role.this: role.name => role }
+
+  name = each.value.name
+  policy =<<EOH
+path "${each.value.backend}/creds/${each.value.name}" {
+    capabilities = ["read"]
+}
+EOH 
+}
