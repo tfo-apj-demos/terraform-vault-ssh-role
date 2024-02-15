80cfdc6f78db667cc76186d002e9d3a3ca98583d
Grant Orchard <go@hashicorp.com>
2024-02-16 08:57:14 +1100

    allow token passthrough

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "credential_paths" {
   value = [ for role in vault_database_secret_backend_role.this: "${role.backend}/creds/${role.name}" ]
 }
+
+output "token" {
+  value = vault_token.this.client_token
+  sensitive = true
+}
