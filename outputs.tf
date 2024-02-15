47439bdde80b7ec50df17b273fc0897cd5f04ac6
Grant Orchard <go@hashicorp.com>
2024-02-15 17:35:50 +1100

    fix output value

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "role_paths" {
-  value = [ for role in vault_database_secret_backend_role.this: "${vault_database_secret_backend_role.this.backend}/creds/${vault_database_secret_backend_role.this.name}" ]
+  value = [ for role in vault_database_secret_backend_role.this: "${role.backend}/creds/${role.name}" ]
 }
