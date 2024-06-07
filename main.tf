0683a604fd2f922c403f95071e8aa0f49b6e845d
Grant Orchard <go@hashicorp.com>
2024-06-07 16:13:51 +1000

    update variables

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ locals {}
 
 resource "vault_ssh_secret_backend_role" "this" {
   backend = var.ssh_mount_path
-  name                    = "my-role"
+  name                    = var.ssh_role_name
   key_type                = "ca"
   allow_user_certificates = true
   # allowed_user_key_config {
   #   type    = "ed25519"
   #   lengths = [2048, 4096]
   # }
-  default_user = "ubuntu"
-  allowed_users = "*"
+  default_user = var.ssh_default_user #"ubuntu"
+  allowed_users = var.ssh_allowed_users #"*"
   ttl = "28800"
   max_ttl = "28800"
   default_extensions = {"permit-pty"=""}
@@ -18,12 +18,10 @@ resource "vault_ssh_secret_backend_role" "this" {
 
 
 resource "vault_policy" "this" {
-  for_each = { for role in vault_database_secret_backend_role.this: role.name => role }
-
   name = each.value.name
   policy =<<EOH
-path "${vault_ssh_secret_backend_role.this.backend}/creds/${each.value.name}" {
-    capabilities = ["read"]
+path "${vault_ssh_secret_backend_role.this.backend}/sign/${vault_ssh_secret_backend_role.this.name}" {
+    capabilities = ["update"]
 }
 EOH 
 }
@@ -32,8 +30,7 @@ resource "vault_token" "this" {
   no_parent = true
   period    = "24h"
   policies = concat([
-    for policy in vault_policy.this: policy.name 
-  ],[
+    vault_policy.this.name,
     "revoke_lease"
   ])
   metadata = {
--- variables.tf
+++ variables.tf
@@ -2,42 +2,15 @@ variable "ssh_mount_path" {
   type = string
   default = "ssh"
 }
-variable "database_connection_name" {
+variable "ssh_role_name" {
   type = string
 }
-variable "database_username" {
+variable "ssh_default_user" {
   type = string
+  default = "ubuntu"
 }
-variable "database_password" {
+variable "ssh_allowed_users" {
   type = string
-  default = ""
-}
-variable "database_addresses" {
-  type = list(string)
-}
-variable "database_port" {
-  type = number
-  default = 5432
-}
-variable "database_name" {}
-variable "database_sslmode" {
-  type = string
-  default = "prefer"
-  validation {
-    condition = contains([
-      "disable",
-      "allow",
-      "prefer",
-      "require",
-      "verify-ca",
-      "verify-full"
-    ], var.database_sslmode)
-    error_message = "database_sslmode value should be one of: disable, allow, prefer, require, verify-ca, or verify-full."
-  }
-}
-variable "database_roles" {
-  type = list(object({
-    name = string
-    creation_statements = list(string)
-  }))
+  description = "Comma separated string of permitted usernames to authenticate as."
+  default = "*"
 }
